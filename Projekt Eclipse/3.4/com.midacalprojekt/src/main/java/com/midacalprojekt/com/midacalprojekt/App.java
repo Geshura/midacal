5c8908ebddd6e9774978e0addb879ae1fdc475d9
logneser <logneser@gmail.com>
2026-01-11 11:36:16 +0100

    OHO

---
+++ Projekt Eclipse/3.4/com.midacalprojekt/src/main/java/com/midacalprojekt/com/midacalprojekt/App.java
@@ -0,0 +1,10 @@
+package com.midacalprojekt.com.midacalprojekt;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
